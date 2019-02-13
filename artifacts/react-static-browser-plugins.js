4788ba514aee1c2820b14c39fde5d7a8a22f92b2
Aapeli <git@aapelivuorinen.com>
2019-02-13 14:46:02 +1100

    Add react-static and typescript

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,17 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+  location: "/Users/aapeli/git/oneonepsilon/oneonepsilon.com/node_modules/react-static-plugin-typescript",
+  plugins: [],
+  hooks: {}
+},
+{
+  location: "/Users/aapeli/git/oneonepsilon/oneonepsilon.com",
+  plugins: [],
+  hooks: {}
+}]
+
+// Export em!
+export default plugins
